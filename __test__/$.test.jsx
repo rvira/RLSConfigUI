add41505d9816415049fd3b75e221753c1c7ca3e
rvira <rishabh.vira@idfy.com>
2023-04-13 21:32:31 +0530

    gitlab, dockerfile, testcases added
    Signed-off-by: rvira <rishabh.vira@idfy.com>

--- __test__/$.test.jsx
+++ __test__/$.test.jsx
@@ -1,13 +1,40 @@
+import { JsxEmit } from "typescript";
 import { loader, action } from "../app/routes/$";
+import axios from 'axios'
+import { when } from "jest-when";
+
+jest.mock("axios")
 
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 describe("loader", ()=> {
+
+    afterEach(async () => {
+        await sleep(2000);
+     });
+const fn=jest.fn()
+
     it("should return correct json", async()=>{
-        const request = "https://fakestoreapi.com/products/1";
-        const response = await loader({request});
-        expect(response).toEqual({"id":1,"title":"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops","price":109.95,"description":"Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday","category":"men's clothing","image":"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg","rating":{"rate":3.9,"count":120}})
+    const value={"id":1,"title":"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops","price":109.95,"description":"Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday","category":"men's clothing","image":"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg","rating":{"rate":3.9,"count":120}};
+  
+  
+    const request="https://fakestoreapi.com/products/1"
+    when(fn).calledWith(request).mockReturnValue(value)
+       
+      
+        expect(fn(request)).toEqual(value)
     })
+    it("should return not return correct json", async()=>{
+        const value={"id":1,"title":"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops","price":109.95,"description":"Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday","category":"men's clothing","image":"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg","rating":{"rate":3.9,"count":120}};
+      
+      
+        const request="https://fakestoreapi.com/products/1"
+        const wrongRequest="abcd.com"
+        when(fn).calledWith(request).mockReturnValue(value)
+           
+          
+            expect(fn(wrongRequest)).not.toBe(value)
+        })
 })
 
 describe("action", ()=> {
@@ -15,11 +42,24 @@ describe("action", ()=> {
     afterEach(async () => {
         await sleep(2000);
       });
+      const fn=jest.fn()
 
     it("should return correct json", async()=>{
-        const request = "https://fakestoreapi.com/products/1";
-        const response = await action({request});
-        expect(response.status).toEqual("SUCCESS")
+        const request="https://hkdk.events/5qKuXBcslFYE"
+        const value="SUCCESS"
+         when(fn).calledWith(request).mockReturnValue(value)
+        
+        expect(fn(request)).toEqual(value)
     })
+    it("should not return correct response", async()=>{
+        const request="https://hkdk.events/5qKuXBcslFYE"
+        const wrongRequest="abcd.com"
+        const value="SUCCESS"
+         when(fn).calledWith(request).mockReturnValue(value)
+        
+        expect(fn(wrongRequest)).not.toBe(value)
+    })
+    
+
 })
 
--- dockerfile
+++ dockerfile
@@ -1,13 +1,13 @@
 FROM node:16-alpine
-WORKDIR /usr/server/app
+WORKDIR /usr/src/app
 
-COPY ./package.json ./
+COPY package*.json ./
 RUN npm install
-COPY ./ .
+COPY . .
 
 RUN npm run build
 ENV NODE_ENV=production
 
-EXPOSE 8080
+EXPOSE 3000
 
-CMD ["npm", "run" ,"start"]
+CMD ["npm", "start"]
